8907e5fe9482add46605639558ade4546e356ca6
Hwang Bokwang <qhrhkd550@naver.com>
2022-04-08 11:48:04 +0900

    2748 피보나치 수 2

---
+++ 백준/Dynamic Programming/2748 피보나치 수 2.py
@@ -0,0 +1,10 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+n = int(input())
+dp = [i for i in range(n+1)]
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
