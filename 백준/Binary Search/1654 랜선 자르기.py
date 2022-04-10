6669edd3b7b649dff8934243befc918090d7fc10
Hwang Bokwang <qhrhkd550@naver.com>
2022-04-10 13:16:00 +0900

    1654 랜선 자르기

---
+++ 백준/Binary Search/1654 랜선 자르기.py
@@ -0,0 +1,25 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+k, n = map(int, input().split())
+lan = []
+for _ in range(k):
+    lan.append(int(input()))
+
+def binary_search(start, end):
+
+    if start > end:
+        return end
+
+    mid = (start + end) // 2
+    tmp = 0
+    for l in lan:
+        tmp += l//mid
+
+    if tmp >= n:
+        return binary_search(mid+1, end)
+    else:
+        return binary_search(start, mid-1)
+
+print(binary_search(1, max(lan)))
+
