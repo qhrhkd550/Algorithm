7120a4f8054736727aff1e11197b54c6b9676802
Hwang Bokwang <qhrhkd550@naver.com>
2022-04-11 20:12:35 +0900

    11726 2xn 타일링

---
+++ 백준/Dynamic Programming/11726 2xn 타일링.py
@@ -0,0 +1,14 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+n = int(input())
+dp = [0] * (n+1)
+dp[0] = 1
+dp[1] = 1
+if n < 2:
+    print(dp[n])
+else:
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    print(dp[n] % 10007)
