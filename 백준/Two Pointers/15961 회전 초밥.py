de11398bcb2d5d282abb794808704d494c0fe5a5
Hwang Bokwang <qhrhkd550@naver.com>
2022-04-14 13:24:34 +0900

    15961 회전 초밥

---
+++ 백준/Two Pointers/15961 회전 초밥.py
@@ -0,0 +1,34 @@
+'''
+* 아이디어 1
+  - 먹은 초밥은 defaultdict으로 관리한다.
+  - 원형 큐 이므로 end의 범위를 제한하지 않고, %n 연산으로 계속 돌아갈 수 있게 한다.
+  - 모든 start 좌표에서 시작하여 k개씩 보는데 먹은 개수가 최대가 되는 값을 저장한다.
+  - 하나의 start에서 k개를 봤다면 start + 1을 위해 dict의 start 값을 1 빼는데 이때 값이 0이된다면 dict에서 start를 제거한다.
+'''
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+from collections import defaultdict
+n, d, k ,c = map(int, input().split())
+shusi = []
+for _ in range(n):
+    shusi.append(int(input()))
+
+end = 0
+eat = defaultdict(int)
+eat[c] = 1
+max_eat = 0
+for start in range(n):
+    while k > 0:
+        eat[shusi[end % n]] += 1
+        end += 1
+        k -= 1
+    if max_eat < len(eat):
+        max_eat = len(eat)
+
+    eat[shusi[start]] -= 1
+    if eat[shusi[start]] == 0:
+        del eat[shusi[start]]
+
+    k += 1
+
+print(max_eat)
