2040d921b23ddf7d65f50bdaa5a99ab8a7e1070a
Hwang Bokwang <qhrhkd550@naver.com>
2022-06-23 13:13:41 +0900

    2668 숫자고르기

--- 백준/Graph Traversal/2668 숫자고르기.py
+++ 백준/Graph Traversal/2668 숫자고르기.py
@@ -1,31 +1,41 @@
+'''
+  * 아이디어 1
+    - n개의 노드를 가지는 경우부터 n-1개를 가지는 경우, n-2개를 가지는 경우, ..., 1개를 가지는 경우에 대해 각각 dfs를 수행하게되면 시간초과가 발생한다.
+    - 따라서 해당 인덱스의 값 정보를 edge로 사용하여 그래프를 형성 후, 사이클이 발생하는 모든 노드들이 정답이 된다.
+'''
 import sys
 input = lambda : sys.stdin.readline().rstrip()
 
 n = int(input())
-num = [0] + [int(input()) for _ in range(n)]
+graph = [[] for _ in range(n+1)]
+for i in range(1, n+1):
+    edge = int(input())
+    graph[i].append(edge)
 
-def dfs(i, count, path):
-    if count == i:
-        tmp = sum(path)
-        for p in path:
-            tmp -= num[p]
-        if tmp == 0:
-            return path
+answer = []
+
+def dfs(now, path):
+    for next in graph[now]:
+        if not visit[next]:
+            visit[next] = True
+            dfs(next, path+[next])
+            visit[next] = False
+
+            return
+
+        while path:
+            node = path.pop()
+            answer.append(node)
+            if node == next:
+                break
+
         return
 
-    for j in range(1, n+1):
-        if not visit[j]:
-            visit[j] = True
-            res = dfs(i, count + 1, path + [j])
-            if res is not None:
-                return res
-            visit[j] = False
+visit = [False] * (n+1)
+for i in range(1, n+1):
+    dfs(i, [])
 
-for i in range(n, 0, -1):
-    visit = [False] * (n+1)
-    res = dfs(i, 0, [])
-    if res is not None:
-        print(len(res))
-        for r in res:
-            print(r)
-        break
+answer = sorted(list(set(answer)))
+print(len(answer))
+for a in answer:
+    print(a)
